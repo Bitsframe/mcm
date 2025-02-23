b8eb6f2a564380378087a6c0b4f5e5d11a3ed2f7
annasshah <30356728+annasshah@users.noreply.github.com>
2025-02-23 15:59:20 +0500

    fix: build isssue

--- app/[locale]/login/layout.tsx
+++ app/[locale]/login/layout.tsx
@@ -3,17 +3,16 @@ import ClientLayout from "@/app/[locale]/login/clientlayout";
 import initTranslations from "@/app/i18n";
 
 const i18nNamespaces = ['Login'];
+interface LayoutProps {
+  children: React.ReactNode;
+  params: any;
+}
 
 export default async function Layout({
   children,
   params: { locale },
-}: {
-  children: React.ReactNode;
-  params: { locale: string };
-}) {
-  console.log('locale', locale);
+}: LayoutProps) {
   const { resources } = await initTranslations(locale, i18nNamespaces);
-  console.log('resources', resources);
   return (
     <TranslationsProvider resources={resources} locale={locale} namespaces={i18nNamespaces}>
       <ClientLayout>{children}</ClientLayout>
