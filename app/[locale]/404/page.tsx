01ce7ae3c9c8cc46bdd2188a8b8982e7eca70bde
annasshah <30356728+annasshah@users.noreply.github.com>
2025-03-01 01:14:38 +0500

    fix: tickets
    TCC-5, B-19

---
+++ app/[locale]/404/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      sfsfsdfdsgsg
+    </div>
+  )
+}
+
+export default page
--- assets/images/index.tsx
+++ assets/images/index.tsx
@@ -3,5 +3,5 @@ import Avatar from "./Avatar/avatar.png";
 import clinca_logo from "./Logos/clinca_logo.png";
 import Darklogo from "./Logos/Darklogo.png"
 import Lightlogo from "./Logos/Lightlogo.png"
-
-export { Logo, Avatar, clinca_logo, Darklogo, Lightlogo };
+import Image404 from './404-mcm.png'
+export { Logo, Avatar, clinca_logo, Darklogo, Lightlogo, Image404 };
