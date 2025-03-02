a21678e1b690fc6b18f12a6bbd917c2eb0d87ae0
Abdul Rafay <rafaysheikhsf@gmail.com>
2025-03-02 23:15:56 +0500

    not-found

--- app/[...not-found]/page.tsx
+++ app/[...not-found]/page.tsx
@@ -1,16 +1,50 @@
 'use client'
-import { Image404 } from '@/assets/images'
+import Link from 'next/link'
+import { Logo } from '@/assets/images'
+import { Button } from '@/components/ui/button'
+import Notf from "@/public/assets/Notf.png"
 import Image from 'next/image'
+import { ArrowLeft } from 'lucide-react'
 import React from 'react'
 
-const page = () => {
+const Page = () => {
   return (
-    <div className="h-[100dvh] flex items-center justify-center bg-gray-100 p-4">
-      <div className="px-3 h-full flex items-center justify-center">
-        <Image onClick={() => window.location.href = '/'}    src={Image404} alt="404 Image" className="max-w-full h-auto cursor-pointer" />
+    <div className='bg-[#E1EAF2] text-center flex flex-col items-center justify-center min-h-screen relative'>
+      <div>
+        <Image src={Logo} alt="Logo" className="w-48 opacity-90 mx-auto" />
+      </div>
+
+      <div className='text-4xl font-semibold px-4'>
+        Oops! Looks like this page needs a check-up. Let's get you back on track!
+      </div>
+
+      <div className='relative font-extrabold text-[300px] text-[#C8D2D8] leading-none'>
+        4
+        <span className="relative inline-block w-[180px] h-[180px] md:w-[220px] md:h-[220px]">
+          <span className="absolute inset-0 flex items-center justify-center text-[#C8D2D8] z-0">
+            0
+          </span>
+          <Image 
+            src={Notf} 
+            alt="Zero Icon" 
+            layout="fill" 
+            objectFit="contain"
+            className="absolute inset-0 z-10"
+          />
+        </span>
+        4
+      </div>
+
+      <div className="flex items-center gap-4 mt-4">
+        <Link href="/" passHref>
+          <Button className='bg-[#C8D2D8] text-black border-2 border-black rounded-full transition-colors duration-300 hover:bg-[#C8D2D8] hover:text-black flex items-center gap-2'>
+            <ArrowLeft size={20} />
+            Home
+          </Button>
+        </Link>
       </div>
     </div>
   )
 }
 
-export default page
+export default Page
