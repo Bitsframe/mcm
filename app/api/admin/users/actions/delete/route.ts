7223a5fa9ca48aed07c78ec137171fa237ad0327
annasshah <30356728+annasshah@users.noreply.github.com>
2025-03-25 04:30:34 +0500

    update key variable

--- app/api/admin/users/actions/delete/route.ts
+++ app/api/admin/users/actions/delete/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 export const POST = async (req: Request) => {
     const supabaseAdmin = createClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.SUPABASE_SERVICE_ROLE_KEY!
+        process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!!
     );
 
     try {
