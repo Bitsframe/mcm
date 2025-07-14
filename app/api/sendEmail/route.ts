e40fc3cfb502aa61b7698e029ba44d0c4cf3e31e
Raheel Hussain <83350399+rnco123@users.noreply.github.com>
2025-07-14 19:22:57 +0500

    Email change in rou.ts in send email

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -138,7 +138,7 @@ export async function POST(req: any) {
 
     // Create a payload that includes all recipients
     const payload = {
-      from: process.env.SENDER_BROADCAST_EMAIL || "test@alerts.myclinicmd.com",
+      from: process.env.SENDER_BROADCAST_EMAIL || "ClinicaSanMiguel@alerts.myclinicmd.com",
       recipients: email.map((recipient: any) => recipient.email),
       subject,
       html: emailHtmls,
