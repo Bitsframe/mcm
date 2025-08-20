b96ad1230f0b58c9fb24cf82624aecc8dac34081
Aleeza Hussain <aleezahussain237@gmail.com>
2025-08-20 20:25:31 +0500

    discount done

--- app/[locale]/(root)/(childroot)/pos/(root)/sales/(root)/page.tsx
+++ app/[locale]/(root)/(childroot)/pos/(root)/sales/(root)/page.tsx
@@ -59,6 +59,7 @@ interface CartItemComponentInterface {
 
 interface CartArrayInterface {
   product_id: number;
+  main_product_id: number;
   quantity: number;
   product_name: string;
   category_name: string;
@@ -538,6 +539,7 @@ const addToCartHandle = () => {
     // Prepare the product data to be added to the cart
     const addProduct: CartArrayInterface = {
       product_id: selectedProduct.product_id,
+      main_product_id: selectedProduct.main_product_id,
       product_name: selectedProduct.product_name,
       quantity: productQty,
       category_name: findCategory.category_name,
