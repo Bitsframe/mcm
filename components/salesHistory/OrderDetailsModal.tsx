c624f5489c289348105afa8f68ea46626b0ccdef
aleeza <aleezahussain237@gmail.com>
2025-10-24 03:15:19 +0500

    ui issue fix

--- components/salesHistory/OrderDetailsModal.tsx
+++ components/salesHistory/OrderDetailsModal.tsx
@@ -298,12 +298,6 @@ const OrderDetailsModal: React.FC<OrderDetailsModalProps> = ({
                       </div>
                     </div>
                     
-                    <div className="flex justify-between">
-                      <span className="text-gray-600 dark:text-gray-400">Credit Balance:</span>
-                      <span className="font-medium text-green-600 dark:text-green-400">
-                        ${(dataList?.credit_balance || 0).toFixed(2)}
-                      </span>
-                    </div>
                   </div>
                 </div>
               </div>
