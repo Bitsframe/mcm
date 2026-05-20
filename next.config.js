45458a9fcc697fe448b09efb873155bbcca2672b
Mavra Iqbal Muccadam <mavra.iqbal@muccadam.com>
2026-05-20 14:10:46 +0500

    qa brach push

--- next.config.js
+++ next.config.js
@@ -1,21 +1,33 @@
 /** @type {import('next').NextConfig} */
+const { codeInspectorPlugin } = require("code-inspector-plugin");
 // next.config.js
 nextConfig = {
-    images: {
-        domains: [
-            'vsvueqtgulraaczqnnvh.supabase.co', // Production domain
-            'yktnfcrxtujyoabjtdff.supabase.co',  // Staging domain
-            'kxtbfmlatysaxjzppmti.supabase.co'   // Child database domain
-        ],
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/cronitor-api/:path*',
-                destination: 'https://cronitor.io/api/:path*', // Proxy to external API
-            },
-        ];
-    },
+  webpack: (config, { dev, isServer }) => {
+    // Only enable in development mode
+    if (dev) {
+      config.plugins.push(
+        codeInspectorPlugin({
+          bundler: "webpack", // Next.js uses webpack
+        }),
+      );
+    }
+    return config;
+  },
+  images: {
+    domains: [
+      "vsvueqtgulraaczqnnvh.supabase.co", // Production domain
+      "yktnfcrxtujyoabjtdff.supabase.co", // Staging domain
+      "kxtbfmlatysaxjzppmti.supabase.co", // Child database domain
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/cronitor-api/:path*",
+        destination: "https://cronitor.io/api/:path*", // Proxy to external API
+      },
+    ];
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
